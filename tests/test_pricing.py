4549497d210199bbc0fa8585ed70c8d982486b58
karanheera <karanheera@gmail.com>
2026-08-17 15:24:39 +0530

    Fix failing pricing test

--- tests/test_pricing.py
+++ tests/test_pricing.py
@@ -1,7 +1,7 @@
 from order_app.pricing import calculate_total
 
 def test_calculate_total():
-    assert calculate_total(500, 3) == 999
+    assert calculate_total(500, 3) == 1500
 
 def test_calculate_total_with_discount():
     assert calculate_total(100, 10) == 900
